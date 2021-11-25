8a7690d4e2f40de24a21abf38d092aa590488115
Abhay Kumar <abbehki@gmail.com>
2021-11-25 22:45:54 +0530

    allpalindrome

---
+++ allpalindrome.js
@@ -0,0 +1,29 @@
+function isPalindrome(str){
+    let str2 =str.split('').reverse().join('')
+    return str == str2
+}
+
+function longestPalindrome(str){
+    let strArr=[];
+    let max_length=0
+    let maxp=''
+    for(let i=0;i < str.length;i++){
+            let subs=str.substring(i);
+        for(let j=subs.length;j>=0;j--){
+           var sub_subs_str = subs.substr(0, j);
+        //   console.log(sub_subs_str)
+          if (isPalindrome(sub_subs_str)){
+            //Below comment code to fine the longest palindromes 
+                // if (sub_subs_str.length > max_length){
+                //     max_length = sub_subs_str.length;
+                //     maxp = sub_subs_str;
+                // }
+                
+                if(sub_subs_str.length > 2){
+                    strArr.push(sub_subs_str)
+                }
+            }
+        }
+    }
+    return strArr;
+}
